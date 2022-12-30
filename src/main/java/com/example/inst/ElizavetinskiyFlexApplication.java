3bcbf03dd54d9fc69d2c1a0640eb5f9517ae9c57
Vasiliy <vasya2304@bk.ru>
2022-12-30 05:02:32 +0300

    Initial commit

---
+++ src/main/java/com/example/inst/ElizavetinskiyFlexApplication.java
@@ -0,0 +1,13 @@
+package com.example.inst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElizavetinskiyFlexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElizavetinskiyFlexApplication.class, args);
+	}
+
+}
