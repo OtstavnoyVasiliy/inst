e10d35097704eb76d4130314df9efc574e612e36
Vasiliy <vasya2304@bk.ru>
2022-12-30 06:51:27 +0300

    created password nd email validators

---
+++ src/main/java/com/example/inst/validations/PasswordMatchesValidator.java
@@ -0,0 +1,20 @@
+package com.example.inst.validations;
+
+import com.example.inst.annotations.PasswordMatches;
+import com.example.inst.payload.request.SignupRequest;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class PasswordMatchesValidator implements ConstraintValidator<PasswordMatches, Object> {
+    @Override
+    public void initialize(PasswordMatches constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(Object obj, ConstraintValidatorContext constraintValidatorContext) {
+        SignupRequest userSignupRequest = (SignupRequest) obj;
+        return userSignupRequest.getPassword().equals(userSignupRequest.getConfirmPassword());
+    }
+}
